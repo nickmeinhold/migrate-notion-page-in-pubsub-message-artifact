f13fefc60ab4cdb9204ac64b21aed392e6aff8a5
Nick Meinhold <nick@enspyr.co>
2023-08-12 02:21:33 +1000

    Squashed 'subtrees/migrate_notion_page_in_pubsub_message/' content from commit 094ab27
    git-subtree-dir: subtrees/migrate_notion_page_in_pubsub_message
    git-subtree-split: 094ab2794594a26d3a9e991138912c6c59b98f75

---
+++ bin/server.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:discord_application_utils/discord_application_utils.dart';
+import 'package:json_utils/json_utils.dart';
+import 'package:notion_api_client/notion_api_client.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_gcp_utils/shelf_gcp_utils.dart';
+
+String? tokenForNotion = Platform.environment['NOTION_TOKEN'];
+
+Future<Response> handler(Request request) async {
+  try {
+    verifyEnvironmentState();
+
+    // Retrieve and log the request body
+    String bodyString = await request.readAsString();
+    print(bodyString);
+
+    // Extract the Pub/Sub message and use it to make an object that will
+    // handle the Discord interaction
+    JsonMap pubsubMessage = extractPubSubMessage(bodyString);
+    final interactor = DiscordInteractor(interactionJson: pubsubMessage);
+
+    // Retrieve the Notion page URL, which was given as a slash command option
+    String pageUrl = interactor.getOptionValue<String>(name: 'url')!;
+
+    var client = NotionClient(token: tokenForNotion!);
+
+    return Response.ok('Success.');
+  } catch (e, s) {
+    print('Exception:\n$e\n\nTrace:\n$s');
+    return Response.internalServerError();
+  }
+}
+
+void main(List<String> args) async {
+  shelf_io.serve(handler, '0.0.0.0', 8080).then((server) {
+    print('Serving at https://${server.address.host}:${server.port}');
+  });
+}
+
+void verifyEnvironmentState() {
+  if (tokenForNotion == null) throw 'Please set "NOTION_TOKEN" env var.';
+}
